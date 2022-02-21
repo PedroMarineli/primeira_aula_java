e68ab1fca34e0b4e0f7969703443bab562bf393c
PedroMarineli <phmo.1606@gmail.com>
2022-02-21 16:29:30 +0000

    ,mç.

--- ola.java
+++ ola.java
@@ -1,9 +1,9 @@
 public class ola{
-    private static float soma(float a, int b) {
+    private static int soma(int a, int b) {
         return a + b;
     }
     public static void main(String[] args){
         System.out.println("Olá Mundo!!!");
-        System.out.println(soma(3.5, 5));
+        System.out.println(soma(3, 5));
     }
 }
