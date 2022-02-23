8beab924120c1435e7004ab8eeeacf28ec25dd66
PedroMarineli <phmo.1606@gmail.com>
2022-02-23 17:10:12 +0000

    kj

--- index.java
+++ index.java
@@ -1,7 +1,13 @@
 public class ola{
     private static int soma(int a, int b) {
-        return a + b;
+        if(a>30){
+            return a + b;      
+        }else{
+            return a - b;
+        }
+     
     }
+
     public static void main(String[] args){
         System.out.println("Olá Mundo!!!");
         System.out.println(soma(3, 5));
